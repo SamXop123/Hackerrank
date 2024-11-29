9d4a8b02dc04508c361c409a1ecacee1db564e3a
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-11-29 18:07:59 +0530

    22. Designer Door Mat

---
+++ Python/22. Designer Door Mat .py
@@ -0,0 +1,16 @@
+# It's easy if you can predict the pattern behind it
+
+n, m = map(int, input().split())
+
+#Top half
+for i in range(1, n, 2):
+    pattern = ".|." * i
+    print(pattern.center(m, "-"))
+
+#Welcome
+print("WELCOME".center(m, "-"))
+
+#Bottom half 
+for i in range(n-2, 0, -2):
+    pattern = ".|." * i
+    print(pattern.center(m, "-"))
