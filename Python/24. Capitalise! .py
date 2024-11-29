70e79826a50432f1f89c71e22027897b47dcb3ec
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-11-29 18:18:20 +0530

    24. Capitalise!

---
+++ Python/24. Capitalise! .py
@@ -0,0 +1,22 @@
+
+# Honestly I would just recommend you to skip this problem.
+# But if you still want to do this then here is the solution from discussions section...
+
+# Try any of the following:
+
+#1
+s = input()
+for x in s[:].split():
+    s = s.replace(x, x.capitalize())
+print(s)
+
+#2
+' '.join(map(str.capitalize, string.split(' ')))
+
+#3
+def solve(s):
+   s = s.split(" ")
+   return(" ".join(i.capitalize() for i in s))
+
+#4
+print(' '.join([x.capitalize() for x in input().split(' ')]))
