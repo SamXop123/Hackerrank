54f18bd729609ebb82348efa7ee87c8cb891808b
Sameer Prajapati <sameerprajapati066@gmail.com>
2024-12-04 00:56:09 +0530

    9. Find the Runner-Up Score!

--- Python/9. Find the Runner-Up Score! .py
+++ Python/9. Find the Runner-Up Score! .py
@@ -4,9 +4,10 @@ if __name__ == '__main__':
  
     arr = list(arr)
  
-    # remove duplicates and sort the arr in descending order
-    unique_arr = sorted(set(arr), reverse=True)
-    
+    # remove duplicates by creating set and sort the arr in descending order
+    unique_arr = list(set(arr))
+    unique_arr.sort(reverse=True)
+
     # the runner-up score is the second element in the sorted list of unique arr
     runner_up_score = unique_arr[1]
     
