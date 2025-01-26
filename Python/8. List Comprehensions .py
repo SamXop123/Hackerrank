dc7582fb6a30467e3e0c44ffa80868213c5b7502
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-01-27 00:55:43 +0530

    Update 8. List Comprehensions .py

--- Python/8. List Comprehensions .py
+++ Python/8. List Comprehensions .py
@@ -3,7 +3,8 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    
+
+# -------------------------
 
 array = [
     [i, j, k] for i in range(x+1) 
@@ -11,5 +12,17 @@ array = [
     for k in range(z+1) 
     if (i + j + k) != n  
 ]
+
+# OR 
+"""
+array = []
+for i in range(x + 1):
+    for j in range(y + 1):
+        for k in range(z + 1):
+            if (i + j + k) != n:
+                array.append([i, j, k])
+"""
+
+# --------------------------------------
 
 print(array)
